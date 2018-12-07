09a491913adffa23cb5577afe89fccc242d45928
RyuTribal <ivan.sedelkin9@gmail.com>
2018-12-07 16:02:51 +0100

    not everything was added

--- routers/register.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var register_controller = require('../controllers/register');
-
-router.post('/', register_controller.userRegister);
-module.exports = router;
